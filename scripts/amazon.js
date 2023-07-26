758c1f0dbf61f5bb16f8cc9e1c61eb0f9dcf1695
Michaelojok11 <michaelocan11@gmail.com>
2023-07-26 13:24:12 +0300

    update git

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -16,14 +16,24 @@ const products = [{
     // Price should be saved as cents
 }, {
     // second product
-    image:'images/ratings/rating-45.png',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    image:'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
     rating: {
-        stars: 4.5,
-        count: 87,
+        stars: 4,
+        count: 127,
     },
-    priceCents: 1090
-    
+    priceCents: 2095    
+},
+
+{
+    // third product
+    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating: {
+        stars: 4,
+        count: 56,
+    },
+    priceCents: 799    
 }
 
 ];
