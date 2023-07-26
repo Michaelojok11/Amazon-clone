4b4b48989442508e7a78b5c52a855781e791b104
Michaelojok11 <michaelocan11@gmail.com>
2023-07-26 14:24:26 +0300

    update git

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+
+// Step 1- Save the data (create a javascript that closely matches the page)
 // we start by saving the data in a variable
 // We have to create an array since it represents a list
 // Then an object since it lets us group multiple values together
@@ -35,5 +37,74 @@ const products = [{
     },
     priceCents: 799    
 }
-
 ];
+// End of saving the data
+
+// Step 2, use the data to generate the HTML
+// To generate the HTML, We can loop through the arrays to create HTML for each of the products
+// 
+
+// step 2 - Generate the HTML Using javascript
+// Looping through the array
+products.forEach(
+    // create a function here. for each takes each product and saves it in a parameter called product, then runs the function
+    (product) => {
+        // for each of these products we want to create some HTML
+        // Therefore we create a variable and we use a template string using back ticks
+        const html = `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+           ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars*10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${product.priceCents / 100}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+
+        `;
+        console.log(html);
+    });
+
+
+    // Combine all the HTML into one and put it on the webpage
+    // 
