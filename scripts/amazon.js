fab8f38a7cf9f1218f0129cc6fcec397510ee664
Michaelojok11 <michaelocan11@gmail.com>
2023-07-27 00:51:01 +0300

    Use products.js files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -4,52 +4,52 @@
 // We have to create an array since it represents a list
 // Then an object since it lets us group multiple values together
 // The object should be created inside an array
-const products = [{
-    // create an object 
-    // first product
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87,
-    },
-    priceCents: 1090
-    // Javascript has challenges dealing with decimals
-    // Price should be saved as cents
-}, {
-    // second product
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127,
-    },
-    priceCents: 2095    
-},
+// const products = [{
+//     // create an object 
+//     // black and Gray Athletic cotton
+//     image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//         stars: 4.5,
+//         count: 87,
+//     },
+//     priceCents: 1090
+//     // Javascript has challenges dealing with decimals
+//     // Price should be saved as cents
+// }, {
+//     // Intermediate Size basketball
+//     image:'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating: {
+//         stars: 4,
+//         count: 127,
+//     },
+//     priceCents: 2095    
+// },
 
-{
-    // third product
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4,
-        count: 56,
-    },
-    priceCents: 799    
-}, 
+// {
+//     // Adults plain cotton T-shirt- 2 pack
+//     image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 4,
+//         count: 56,
+//     },
+//     priceCents: 799    
+// }, 
 
-{
-    // third product
-    image:'images/products/black-2-slot-toaster.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4,
-        count: 56,
-    },
-    priceCents: 799    
-}, 
+// {
+//     // slot toaster -black
+//     image:'images/products/black-2-slot-toaster.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 5,
+//         count: 2197,
+//     },
+//     priceCents: 1899    
+// }, 
 
-];
+// ];
 // End of saving the data
 
 // step 3- step 2 is below step 3, combine all the HTML in one 
